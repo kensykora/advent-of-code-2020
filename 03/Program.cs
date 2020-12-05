09721fa8024ba1da5b1aa8c07707918150f24175
Ken Sykora <ksykora@ostusa.com>
2020-12-05 12:16:09 -0600

    Day 3 pt 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -7,16 +7,18 @@ namespace _03
 {
     class Program
     {
-        const int Right = 3;
-        const int Down = 1;
 
         static void Main(string[] args)
         {
-            Part1();
+            //Part1();
+            Part2();
         }
 
         static void Part1()
         {
+            const int Right = 3;
+            const int Down = 1;
+
             var input = File.ReadLines("input.txt");
             var rows = new List<bool[]>();
             foreach (var line in input)
@@ -30,12 +32,12 @@ namespace _03
             var width = map[0].Length;
             var treesEncountered = 0;
 
-            for (var i = 0; i < map.Length-1; i++) // For each row, except the last
+            for (var i = 0; i < map.Length - 1; i++) // For each row, except the last
             {
                 current.x += Right;
                 current.y += Down;
 
-                if(map[current.y][current.x % width])
+                if (map[current.y][current.x % width])
                 {
                     treesEncountered++;
                 }
@@ -43,5 +45,54 @@ namespace _03
 
             Console.WriteLine("Trees: " + treesEncountered);
         }
+
+        static void Part2()
+        {
+            var input = File.ReadLines("input.txt");
+            var rows = new List<bool[]>();
+            foreach (var line in input)
+            {
+                rows.Add(line.Select(x => x == '#').ToArray());
+            }
+
+            var map = rows.ToArray();
+            var width = map[0].Length;
+
+            var runs = new List<SlopeRun>()
+            {
+                new SlopeRun { Right = 1, Down = 1 },
+                new SlopeRun { Right = 3, Down = 1 },
+                new SlopeRun { Right = 5, Down = 1 },
+                new SlopeRun { Right = 7, Down = 1 },
+                new SlopeRun { Right = 1, Down = 2 }
+            };
+
+            foreach (var run in runs)
+            {
+                while (run.CurrentY + run.Down < map.Length)
+                {
+                    run.CurrentX += run.Right;
+                    run.CurrentY += run.Down;
+
+                    if (map[run.CurrentY][run.CurrentX % width])
+                    {
+                        run.TreesEncountered++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Result: " + runs.Aggregate(
+                (result, item) => new SlopeRun() { TreesEncountered = result.TreesEncountered * item.TreesEncountered }).TreesEncountered);
+        }
+    }
+
+    public class SlopeRun
+    {
+
+        public int CurrentX { get; set; }
+        public int CurrentY { get; set; }
+        public int Right { get; set; }
+        public int Down { get; set; }
+        public uint TreesEncountered { get; set; }
     }
 }
