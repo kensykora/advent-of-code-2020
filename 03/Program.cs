e5b4291f530ee933233ea0aa22755324204f2394
Ken Sykora <ksykora@ostusa.com>
2020-12-05 11:58:25 -0600

    Day 3 pt 1

---
+++ 03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        const int Right = 3;
+        const int Down = 1;
+
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadLines("input.txt");
+            var rows = new List<bool[]>();
+            foreach (var line in input)
+            {
+                rows.Add(line.Select(x => x == '#').ToArray());
+            }
+
+            var map = rows.ToArray();
+
+            var current = (x: 0, y: 0);
+            var width = map[0].Length;
+            var treesEncountered = 0;
+
+            for (var i = 0; i < map.Length-1; i++) // For each row, except the last
+            {
+                current.x += Right;
+                current.y += Down;
+
+                if(map[current.y][current.x % width])
+                {
+                    treesEncountered++;
+                }
+            }
+
+            Console.WriteLine("Trees: " + treesEncountered);
+        }
+    }
+}
