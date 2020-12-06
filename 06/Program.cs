d54285a0cf0be9051b21c95e4eb6ccc67d881e71
Ken Sykora <ksykora@ostusa.com>
2020-12-06 07:18:52 -0600

    Day 6 Pt 1

---
+++ 06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllText("input.txt");
+            var groups = input.Split("\r\n\r\n");
+            var groupData = groups.Select(x => x.Replace("\r\n", null).Distinct());
+            
+            var count = groupData.Sum(x => x.Count());
+            Console.WriteLine(count);
+        }
+    }
+}
