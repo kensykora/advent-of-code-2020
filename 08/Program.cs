8ef1b2061ede4dec211647cc23fa240828b7ea56
Ken Sykora <ksykora@ostusa.com>
2020-12-13 09:59:37 -0600

    Day 8 pt 2

--- 08/Program.cs
+++ 08/Program.cs
@@ -9,11 +9,6 @@ namespace _08
         static Instruction[] Instructions;
         static void Main(string[] args)
         {
-            Part1();
-        }
-
-        static void Part1()
-        {
             Instructions =
                 File.ReadAllLines("input.txt")
                 .Select(x => new Instruction
@@ -23,15 +18,68 @@ namespace _08
                 })
                 .ToArray();
 
-            Console.WriteLine(GetValue());
+            Part1();
+            Part2();
         }
 
-        static int GetValue()
+        static void Part1()
+        {
+            Console.WriteLine(GetValue().result);
+        }
+
+        static void Part2()
+        {
+            var jmps = Instructions.Where(x => x.Op == Operation.jmp);
+            foreach(var jmp in jmps)
+            {
+                jmp.Op = Operation.nop;
+
+                var newResult = GetValue();
+
+                if (!newResult.didLoop)
+                {
+                    Console.WriteLine(newResult.result);
+                    return;
+                }
+
+                jmp.Op = Operation.jmp;
+                
+                foreach(var i in Instructions)
+                {
+                    i.Visited = false;
+                }
+            }
+
+            var nops = Instructions.Where(x => x.Op == Operation.nop);
+            foreach(var nop in nops)
+            {
+                nop.Op = Operation.jmp;
+
+                var newResult = GetValue();
+
+                if (!newResult.didLoop)
+                {
+                    Console.WriteLine(newResult.result);
+                    return;
+                }
+
+                nop.Op = Operation.nop;
+
+                foreach(var i in Instructions)
+                {
+                    i.Visited = false;
+                }
+            }
+
+            Console.WriteLine("Did not find");
+        }
+
+        static (int result, bool didLoop) GetValue()
         {
             var result = 0;
             var current = 0;
 
-            while(!Instructions[current].Visited)
+            while(current < Instructions.Length && !Instructions[current].Visited)
             {
                 var instruction = Instructions[current];
                 instruction.Visited = true;
@@ -51,7 +99,7 @@ namespace _08
                 }
             }
 
-            return result;
+            return (result, current < Instructions.Length);
         }
     }
 
