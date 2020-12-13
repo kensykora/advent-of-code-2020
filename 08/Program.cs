a4737ad19590eddb7995404dc4db0a04978258b6
Ken Sykora <ksykora@ostusa.com>
2020-12-13 09:36:41 -0600

    Day 8 pt 1

---
+++ 08/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace _08
+{
+    class Program
+    {
+        static Instruction[] Instructions;
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        static void Part1()
+        {
+            Instructions =
+                File.ReadAllLines("input.txt")
+                .Select(x => new Instruction
+                {
+                    Op = (Operation)Enum.Parse(typeof(Operation), x.Substring(0, 3)),
+                    Value = int.Parse(x.Substring(4))
+                })
+                .ToArray();
+
+            Console.WriteLine(GetValue());
+        }
+
+        static int GetValue()
+        {
+            var result = 0;
+            var current = 0;
+
+            while(!Instructions[current].Visited)
+            {
+                var instruction = Instructions[current];
+                instruction.Visited = true;
+
+                switch (instruction.Op)
+                {
+                    case Operation.jmp:
+                        current += instruction.Value;
+                        break;
+                    case Operation.acc:
+                        result += instruction.Value;
+                        current += 1;
+                        break;
+                    case Operation.nop:
+                        current += 1;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+
+
+    public class Instruction
+    {
+        public Operation Op { get; set; }
+        public bool Visited { get; set; }
+        public int Value { get; set; }
+    }
+
+    public enum Operation
+    {
+        nop,
+        acc,
+        jmp
+    }
+}
