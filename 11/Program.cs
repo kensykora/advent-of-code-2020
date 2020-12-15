60d8cdf87464daf1eef049a90e61b544fd3c3fb3
Ken Sykora <ksykora@ostusa.com>
2020-12-15 17:46:59 -0600

    Day11 pt2, day 12 pt 1

--- 11/Program.cs
+++ 11/Program.cs
@@ -22,6 +22,7 @@ namespace _11
             }
 
             Part1(map);
+            Part2(map);
         }
 
         static void Part1(bool?[,] map)
@@ -37,6 +38,25 @@ namespace _11
                 oldCount = newCount;
                 newCount = result.occupiedCount;
                 current = result.result;
+            }
+
+            Console.WriteLine(newCount);
+        }
+
+        static void Part2(bool?[,] map)
+        {
+            var oldCount = -1;
+            var newCount = 0;
+
+            var current = map;
+
+            while (oldCount != newCount)
+            {
+                var result = ApplyRoundPart2(current);
+                oldCount = newCount;
+                newCount = result.occupiedCount;
+                current = result.result;
+                //Print(current);
             }
 
             Console.WriteLine(newCount);
@@ -88,6 +108,68 @@ namespace _11
             return (result, occupiedCount);
         }
 
+        static (bool?[,] result, int occupiedCount) ApplyRoundPart2(bool?[,] map)
+        {
+            var result = new bool?[map.GetLength(0), map.GetLength(1)];
+            var occupiedCount = 0;
+            for (var i = 0; i < result.GetLength(0); i++)
+            {
+                for (var j = 0; j < result.GetLength(1); j++)
+                {
+                    if (map[i, j] == false) // If not occupied
+                    {
+                        if (CountUnendingOccupiedSeats(map, i, j) == 0)
+                        {
+                            // become occupied
+                            result[i, j] = true;
+                            occupiedCount++;
+                        }
+                        else
+                        {
+                            // stay unoccupied
+                            result[i, j] = false;
+                        }
+                    }
+                    else if (map[i, j] == true) // If occupied
+                    {
+                        if (CountUnendingOccupiedSeats(map, i, j) >= 5)
+                        {
+                            // become unoccupied
+                            result[i, j] = false;
+                        }
+                        else
+                        {
+                            // stay occupied
+                            result[i, j] = true;
+                            occupiedCount++;
+                        }
+                    }
+                    else
+                    {
+                        result[i, j] = null;
+                    }
+                }
+            }
+
+            return (result, occupiedCount);
+        }
+
+        static int CountUnendingOccupiedSeats(bool?[,] map, int row, int col)
+        {
+            var result = 0;
+
+            if (SeatOccupiedUnending(map, row, col, -1, -1)) result++;
+            if (SeatOccupiedUnending(map, row, col, -1, 0)) result++;
+            if (SeatOccupiedUnending(map, row, col, -1, 1)) result++;
+            if (SeatOccupiedUnending(map, row, col, 0, -1)) result++;
+            if (SeatOccupiedUnending(map, row, col, 0, 1)) result++;
+            if (SeatOccupiedUnending(map, row, col, 1, -1)) result++;
+            if (SeatOccupiedUnending(map, row, col, 1, 0)) result++;
+            if (SeatOccupiedUnending(map, row, col, 1, 1)) result++;
+
+            return result;
+        }
+
         static int CountAdjacentOccupiedSeats(bool?[,] map, int row, int col)
         {
             var result = 0;
@@ -116,6 +198,42 @@ namespace _11
             }
 
             return false;
+        }
+
+        static bool SeatOccupiedUnending(bool?[,] map, int row, int col, int deltaRow, int deltaCol)
+        {
+            var result = false;
+            var currentRow = row + deltaRow;
+            var currentCol = col + deltaCol;
+
+            while (currentRow >= 0 && currentRow < map.GetLength(0)
+                && currentCol >= 0 && currentCol < map.GetLength(1)
+                && !map[currentRow, currentCol].HasValue)
+            {
+                currentRow += deltaRow;
+                currentCol += deltaCol;
+            }
+
+            if (currentRow >= 0 && currentRow < map.GetLength(0)
+                && currentCol >= 0 && currentCol < map.GetLength(1))
+            {
+                result = map[currentRow, currentCol] == true;
+            }
+
+            return result;
+        }
+
+        static void Print(bool?[,] map)
+        {
+            for (var i = 0; i < map.GetLength(0); i++)
+            {
+                for (var j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j] == true ? '#' : (map[i, j] == false ? 'L' : '.'));
+                }
+                Console.Write('\n');
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ 12/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt");
+            Part1(input);
+        }
+
+        static void Part1(IEnumerable<string> input)
+        {
+            var direction = 90;
+            var eastWestPos = 0;
+            var northSouthPos = 0;
+
+            var instructions = input.GetEnumerator();
+
+            while (instructions.MoveNext())
+            {
+                var instruction = instructions.Current[0];
+                var value = int.Parse(instructions.Current.Substring(1));
+
+                switch (instruction)
+                {
+                    case 'N':
+                        northSouthPos += value;
+                        break;
+                    case 'S':
+                        northSouthPos -= value;
+                        break;
+                    case 'E':
+                        eastWestPos += value;
+                        break;
+                    case 'W':
+                        eastWestPos -= value;
+                        break;
+                    case 'L':
+                        value = value % 360;
+                        direction -= value;
+                        direction = NormalizeDirection(direction);
+                        break;
+                    case 'R':
+                        value = value % 360;
+                        direction += value;
+                        direction = NormalizeDirection(direction);
+                        break;
+                    case 'F':
+                        switch (direction)
+                        {
+                            case 0:
+                                northSouthPos += value;
+                                break;
+                            case 90:
+                                eastWestPos += value;
+                                break;
+                            case 180:
+                                northSouthPos -= value;
+                                break;
+                            case 270:
+                                eastWestPos -= value;
+                                break;
+                            default:
+                                throw new ApplicationException("Unexpected direction: " + direction);
+                        }
+                        break;
+                    default:
+                        throw new ApplicationException("Unexpected value: " + instruction);
+                }
+            }
+
+            Console.WriteLine(Math.Abs(northSouthPos) + Math.Abs(eastWestPos));
+        }
+
+        static int NormalizeDirection(int direction)
+        {
+            if (direction >= 360)
+            {
+                direction -= 360;
+            }
+            else if (direction < 0)
+            {
+                direction += 360;
+            }
+
+            return direction;
+        }
+    }
+}
