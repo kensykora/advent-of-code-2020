c211fca0b39cade10271d381efb9acdf9639fc5a
Ken Sykora <ksykora@ostusa.com>
2020-12-15 18:37:13 -0600

    Day 13 pt 1

---
+++ 13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var earliestTime = int.Parse(input[0]);
+            var busses = input[1].Split(',').Where(x => x != "x").Select(int.Parse).ToArray();
+
+            Part1(earliestTime, busses);
+        }
+
+        static void Part1(int earliestTime, int[] busses)
+        {
+            var data = busses
+                .Select(x => new { x, diff = (Math.Ceiling((double)earliestTime / x) * x) - earliestTime })
+                .OrderBy(x => x.diff);
+            var soonest = data.First();
+            Console.WriteLine(soonest.x * soonest.diff);
+        }
+    }
+}
