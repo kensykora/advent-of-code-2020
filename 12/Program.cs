3f58fc1afc45afda3bf6816c620b62dbacb8398a
Ken Sykora <ksykora@ostusa.com>
2020-12-15 18:12:38 -0600

    Day 12 pt 2

--- 12/Program.cs
+++ 12/Program.cs
@@ -11,6 +11,7 @@ namespace _12
         {
             var input = File.ReadLines("input.txt");
             Part1(input);
+            Part2(input);
         }
 
         static void Part1(IEnumerable<string> input)
@@ -68,6 +69,94 @@ namespace _12
                             default:
                                 throw new ApplicationException("Unexpected direction: " + direction);
                         }
+                        break;
+                    default:
+                        throw new ApplicationException("Unexpected value: " + instruction);
+                }
+            }
+
+            Console.WriteLine(Math.Abs(northSouthPos) + Math.Abs(eastWestPos));
+        }
+
+        static void Part2(IEnumerable<string> input)
+        {
+            var eastWestPos = 0;
+            var northSouthPos = 0;
+
+            var waypointNorthSouth = 1;
+            var waypointEastWest = 10;
+
+            var instructions = input.GetEnumerator();
+
+            while (instructions.MoveNext())
+            {
+                var instruction = instructions.Current[0];
+                var value = int.Parse(instructions.Current.Substring(1));
+                var origNorthSouth = 0;
+
+                switch (instruction)
+                {
+                    case 'N':
+                        waypointNorthSouth += value;
+                        break;
+                    case 'S':
+                        waypointNorthSouth -= value;
+                        break;
+                    case 'E':
+                        waypointEastWest += value;
+                        break;
+                    case 'W':
+                        waypointEastWest -= value;
+                        break;
+                    case 'R':
+                        value = value % 360;
+                        origNorthSouth = waypointNorthSouth;
+                        switch(value)
+                        {
+                            case 360:
+                            case 0:
+                                // do nothing
+                                break;
+                            case 90:
+                                waypointNorthSouth = -1 * waypointEastWest;
+                                waypointEastWest = origNorthSouth;
+                                break;
+                            case 180:
+                                waypointNorthSouth = -1 * waypointNorthSouth;
+                                waypointEastWest = -1 * waypointEastWest;
+                                break;
+                            case 270:
+                                waypointNorthSouth = waypointEastWest;
+                                waypointEastWest = -1 * origNorthSouth;
+                                break;
+                        }
+                        break;
+                    case 'L':
+                        value = value % 360;
+                        origNorthSouth = waypointNorthSouth;
+                        switch(value)
+                        {
+                            case 360:
+                            case 0:
+                                // do nothing
+                                break;
+                            case 90:
+                                waypointNorthSouth = waypointEastWest;
+                                waypointEastWest = -1 * origNorthSouth;
+                                break;
+                            case 180:
+                                waypointNorthSouth = -1 * waypointNorthSouth;
+                                waypointEastWest = -1 * waypointEastWest;
+                                break;
+                            case 270:
+                                waypointNorthSouth = -1 * waypointEastWest;
+                                waypointEastWest = origNorthSouth;
+                                break;
+                        }
+                        break;
+                    case 'F':
+                        northSouthPos += value * waypointNorthSouth;
+                        eastWestPos += value * waypointEastWest;
                         break;
                     default:
                         throw new ApplicationException("Unexpected value: " + instruction);
