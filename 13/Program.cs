7f267706590e87b8af37a22f1f8b7fdf90a0b5bd
Ken Sykora <ksykora@ostusa.com>
2020-12-18 17:49:58 -0600

    Day 13 pt 2, Day 14 pt 1

--- 13/Program.cs
+++ 13/Program.cs
@@ -13,6 +13,7 @@ namespace _13
             var busses = input[1].Split(',').Where(x => x != "x").Select(int.Parse).ToArray();
 
             Part1(earliestTime, busses);
+            Part2(input[1].Split(',').Select(x => ulong.TryParse(x, out var i) ? i : (ulong?)null).ToArray());
         }
 
         static void Part1(int earliestTime, int[] busses)
@@ -22,6 +23,78 @@ namespace _13
                 .OrderBy(x => x.diff);
             var soonest = data.First();
             Console.WriteLine(soonest.x * soonest.diff);
+        }
+
+        static void Part2(ulong?[] busses)
+        {
+            var data =
+                busses.Select(x => x == null ? null : new Bus(x.Value)).ToArray();
+            var result = IsSolved(data);
+
+            while (!result.isSolved)
+            {
+                ApplyNextIteration(data);
+                result = IsSolved(data);
+            }
+
+            Console.WriteLine(data[0].Current);
+        }
+
+        static void ApplyNextIteration(Bus[] busses)
+        {
+            ulong minValue = 100000000000000;
+            var reverse = busses.Reverse().ToArray();
+            ulong max = 0;
+            for (var i = 0; i < busses.Length; i++)
+            {
+                if (i == 0)
+                {
+                    reverse[i].Current += reverse[i].Id;
+                    max = reverse[i].Current;
+
+                    if(max < minValue)
+                    {
+                        reverse[i].Current = (ulong)Math.Ceiling((double)minValue / reverse[i].Id) * reverse[i].Id;
+                        max = reverse[i].Current;
+                    }
+                }
+                else if (reverse[i] != null)
+                {
+                    reverse[i].Current = (ulong)Math.Floor((double)(max - (ulong)i) / reverse[i].Id) * reverse[i].Id;
+                }
+            }
+            //Print(busses);
+        }
+
+        static (bool isSolved, Bus incorrectBus) IsSolved(Bus[] busses)
+        {
+            var min = busses[0].Current;
+            for (var i = (ulong)1; i < (ulong)busses.LongLength; i++)
+            {
+                if (busses[i] != null && busses[i].Current != min + i)
+                {
+                    return (false, busses[i]);
+                }
+            }
+
+            return (true, null);
+        }
+
+        public class Bus
+        {
+            public Bus(ulong id)
+            {
+                Id = id;
+
+                Current = 0;
+            }
+            public ulong Id { get; set; }
+            public ulong Current { get; set; }
+        }
+
+        static void Print(Bus[] busses)
+        {
+            Console.WriteLine(string.Join(' ', busses.Select(x => x == null ? "x" : x.Current.ToString())));
         }
     }
 }
---
+++ 14/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt");
+            Part1(input);
+        }
+
+        static (ulong ones, ulong zeroes) GetMask(string mask)
+        {
+            ulong ones = 0;
+            ulong zeroes = ulong.MaxValue;
+            for (var i = 0; i < mask.Length; i++)
+            {
+                var c = mask[i];
+                if (c == '1')
+                {
+                    ones = ones | (ulong)1 << (35 - i);
+                }
+                else if (c == '0')
+                {
+                    zeroes = zeroes & (((ulong.MaxValue - 1) << (35 - i)) | (ulong.MaxValue - 1) >> 64 - (35 - i));
+                }
+            }
+
+            return (ones, zeroes);
+        }
+
+        static void Part1(IEnumerable<string> file)
+        {
+            var values = new Dictionary<ulong, ulong>();
+            var input = file.GetEnumerator();
+            var regex = new Regex("mem\\[(?<address>\\d+)\\] = (?<value>\\d+)");
+            (ulong ones, ulong zeroes) mask = (0, ulong.MaxValue);
+
+            while (input.MoveNext())
+            {
+                if (input.Current.StartsWith("mask = "))
+                {
+                    mask = GetMask(input.Current.Substring("mask = ".Length));
+                }
+                else
+                {
+                    var matches = regex.Match(input.Current);
+                    values[ulong.Parse(matches.Groups["address"].Value)] = (ulong.Parse(matches.Groups["value"].Value) | mask.ones) & mask.zeroes;
+                }
+            }
+
+            Console.WriteLine(values.Values.Aggregate((sum, next) => sum + next));
+        }
+    }
+}
