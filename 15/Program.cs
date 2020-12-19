566e2f0707f1a8798f44140219361f757d115026
Ken Sykora <ksykora@ostusa.com>
2020-12-19 09:02:14 -0600

    Day 15 pt 2

--- 15/Program.cs
+++ 15/Program.cs
@@ -11,7 +11,11 @@ namespace _15
             //Solve("0,3,6", 10); // Sample
             //Solve("1,3,2", 2020); // Sample
 
-            Solve("18,11,9,0,5,1", 2020);
+            //Solve("18,11,9,0,5,1", 2020); // Pt1 input
+
+            //Solve("0,3,6", 30000000); // Sample
+
+            Solve("18,11,9,0,5,1", 30000000); // Pt2 input
         }
 
         static void Solve(string input, int target)
@@ -20,7 +24,7 @@ namespace _15
             //Console.Write(input);
 
             // Key = Number spoken, // Value = Index it references
-            var values = new SortedList<int, (int prev, int current)>();
+            var values = new Dictionary<int, (int prev, int current)>();
 
             for (var i = 0; i < gameLog.Count; i++)
             {
@@ -33,14 +37,14 @@ namespace _15
             for (var j = gameLog.Count; j < target; j++)
             {
                 history = values[gameLog[j - 1]];
-                next = history.prev == -1 
-                    ? 0 
+                next = history.prev == -1
+                    ? 0
                     : history.current - history.prev;
 
                 history = values.ContainsKey(next)
                     ? (values[next].current, j)
                     : (-1, j);
-               
+
                 gameLog.Add(next);
                 values[next] = history;
 
